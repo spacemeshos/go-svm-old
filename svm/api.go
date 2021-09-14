a6954b0483aef894cc1f5caf3f42f996de389c0a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2021-09-14 16:37:48 +0300

    Added comments

--- svm/api.go
+++ svm/api.go
@@ -1,8 +1,26 @@
 package svm
 
+// TODO: we might want to guard calling `Init` with a Mutex.
 var initialized = false
 
-func Init(inMemory bool, path string) {
+// `Init` should be called exactly once before interacting with any other API of SVM.
+// Each future call to `NewRuntime` (see later) assumes the settings given the `Init` call.
+//
+// # Params
+//
+// * `inMemory` - whether the data of the `SVM Global State` will be in-memory or persisted.
+// * `path` 	- the path under which `SVM Global State` will store its content.
+//   This is relevant only when `isMemory=false` (otherwise the `path` value will be ignored).
+//
+// # Returns
+//
+// Returns an error in case the initialization has failed.
+//
+//
+// # Panics
+//
+// Panics when `Init` has already been called.
+func Init(inMemory bool, path string) error {
 	if initialized {
 		panic("`Init` can be called only once")
 	}
@@ -10,69 +28,159 @@ func Init(inMemory bool, path string) {
 	panic("TODO")
 }
 
+// Asserts that `Init` has already been called.
+//
+// # Panics
+//
+// Panics when `Init` has **NOT** been previously called.
 func AssertInitialized() {
 	if !initialized {
 		panic("Forgot to call `Init`")
 	}
 }
 
+// Creates a new `Runtime`.
+//
+// On success returns it and the `error` is set to `nil`.
+// On failure returns `(nil, error).
 func NewRuntime() (*Runtime, error) {
 	panic("TODO")
 }
 
-// Releases SVM runtime
+// Releases the SVM Runtime
 func (rt *Runtime) Destroy() {
 	panic("TODO")
 }
 
-func (rt *Runtime) Close() error {
-	panic("TODO")
-}
-
+// Validates the `Deploy Message` given in its binary form.
+//
+// Returns `(true, nil)` when the `msg` is syntactically valid,
+// and `(false, error)` otherwise.  In that case `error` will have non-`nil` value.
 func (rt *Runtime) ValidateDeploy(msg []byte) (bool, ValidateError) {
 	panic("TODO")
 }
 
+// Executes a `Deploy` transaction and returns back a receipt.
+//
+// # Params
+//
+// * `env` - the transaction `Envelope`
+// * `msg` - the transaction `Message`
+// * `ctx` - the executed `Context` (the `current layer` etc).
+//
+// # Notes
+//
+// A Receipt is always being returned, even if there was an internal error inside SVM.
 func (rt *Runtime) Deploy(env Envelope, msg []byte, ctx Context) DeployReceipt {
 	panic("TODO")
 }
 
+// Validates the `Spawn Message` given in its binary form.
+//
+// Returns `(true, nil)` when the `msg` is syntactically valid,
+// and `(false, error)` otherwise.  In that case `error` will have non-`nil` value.
 func (rt *Runtime) ValidateSpawn(msg []byte) (bool, ValidateError) {
 	panic("TODO")
 }
 
+// Executes a `Spawn` transaction and returns back a receipt.
+//
+// # Params
+//
+// * `env` - the transaction `Envelope`
+// * `msg` - the transaction `Message`
+// * `ctx` - the executed `Context` (the `current layer` etc).
+//
+//
+// # Notes
+//
+// A Receipt is always being returned, even if there was an internal error inside SVM.
 func (rt *Runtime) Spawn(env Envelope, msg []byte, ctx Context) SpawnReceipt {
 	panic("TODO")
 }
 
+// Validates the `Call Message` given in its binary form.
+//
+// Returns `(true, nil)` when the `msg` is syntactically valid,
+// and `(false, error)` otherwise.  In that case `error` will have non-`nil` value.
 func (rt *Runtime) ValidateCall(msg []byte) (bool, ValidateError) {
 	panic("TODO")
 }
 
+// Executes a `Call` transaction and returns back a receipt.
+//
+// # Params
+//
+// * `env` - the transaction `Envelope`
+// * `msg` - the transaction `Message`
+// * `ctx` - the executed `Context` (the `current layer` etc).
+//
+// # Notes
+//
+// A Receipt is always being returned, even if there was an internal error inside SVM.
 func (rt *Runtime) Call(env Envelope, msg []byte, ctx Context) CallReceipt {
 	panic("TODO")
 }
 
+// Executes the `Verify` stage and returns back a receipt.
+//
+// Calling `Verify` is in many ways very similar to the latter `Call` execution.
+// For that reason, the `Verify` returns a `CallReceipt` as well.
+//
+// # Params
+//
+// * `env` - the transaction `Envelope`
+// * `msg` - the transaction `Message`
+// * `ctx` - the executed `Context` (the `current layer` etc).
+//
+// # Notes
+//
+// A Receipt is always being returned, even if there was an internal error inside SVM.
 func (rt *Runtime) Verify(env Envelope, msg []byte, ctx Context) CallReceipt {
 	panic("TODO")
 }
 
-func (rt *Runtime) Open(layer Layer) {
+// Signaling `SVM` that we are about to start playing a list of transactions under the input `layer` Layer.
+//
+// # Notes
+//
+// * The value of `layer` is expected to equal the last known `committed/rewinded` layer plus one
+//   Any other `layer` given as input will result in an error returned.
+//
+// * Calling `Open` twice in a row will result in an `error` returned.
+func (rt *Runtime) Open(layer Layer) error {
 	panic("TODO")
 }
 
-func (rt *Runtime) Rewind(layer Layer) {
+// Rewinds the `SVM Global State` back to the input `layer`.
+//
+// In case there is no such layer to rewind to - returns an `error`.
+func (rt *Runtime) Rewind(layer Layer) error {
 	panic("TODO")
 }
 
-func (rt *Runtime) Commit() Layer {
+// Commits the dirty changes of `SVM` and signals the termination of the current layer.
+// On success returns `(layer, nil)` when `layer` is the value of the previous `current layer`.
+// In other words, returns the `layer` associated with the just-committed changes.
+//
+// In case commits fails (for example, persisting to disk failure) - returns `(0, error)`
+func (rt *Runtime) Commit() (Layer, error) {
 	panic("TODO")
 }
 
-func (rt *Runtime) GetAccount(addr Address) Account {
+// Given an `Account Address` - retrieves its most basic information encapuslated within an `Account` struct.
+//
+// Returns a `(nil, error)` in case the requested `Account` doesn't exist.
+func (rt *Runtime) GetAccount(addr Address) (Account, error) {
 	panic("TODO")
 }
 
-func (rt *Runtime) IncreaseBalance(amount Amount) {
+// Increases the balance of an Account (i.e printing coins)
+//
+// # Params
+//
+// * `addr`   - The `Account Address` we want to increase its balance.
+// * `amount` - The `Amount` by which we are going to increase the account's balance.
+func (rt *Runtime) IncreaseBalance(addr Account, amount Amount) {
 	panic("TODO")
 }
--- svm/types.go
+++ svm/types.go
@@ -2,6 +2,7 @@ package svm
 
 import "unsafe"
 
+// Declaring types aliases used throughout the project.
 type TxType uint8
 type Amount uint64
 type Address [20]byte
@@ -12,27 +13,36 @@ type Gas uint64
 type Layer uint64
 type Log []byte
 
-// Runtime is a wrapper for svm_runtime
+// `Runtime` wraps the raw-Runtime returned by SVM C-API
 type Runtime struct {
 	raw unsafe.Pointer
 }
 
+// Holds the currently executed `Node Context`.
+// Addionally, contains data implied/computed from the `input` transaction.
 type Context struct {
 	Layer Layer
 	TxId  TxId
 }
 
+// Encapsulates an `Account Counter`. (Since `Golang` has no `unit128` primitive out-of-the-box).
+//
+// Used for implementing the `Nonce Scheme` implemented within the `Template` associated with the `Account`
 type AccountCounter struct {
 	Upper uint64
 	Lower uint64
 }
 
+// A `Transaction Type` enum
 const (
 	DeployType TxType = 0
 	SpawnType  TxType = 1
 	CallType   TxType = 2
 )
 
+// Holds the `Envelope` of a transaction.
+//
+// In other words, holds fields which are part of any transaction regardless of its type (i.e `Deploy/Spawn/Call`).
 type Envelope struct {
 	Type      TxType
 	Principal Address
@@ -42,20 +52,23 @@ type Envelope struct {
 	GasFee    uint64
 }
 
+// Holds an `Account` basic information.
 type Account struct {
 	Addr    Address
 	Balance Amount
 	Counter AccountCounter
 }
 
+// Holds the data returned after executing a `Deploy` transaction.
 type DeployReceipt struct {
-	Success bool
-	Error   RuntimeError
-	Addr    TemplateAddr
-	GasUsed Gas
-	Logs    []Log
+	Success      bool
+	Error        RuntimeError
+	TemplateAddr TemplateAddr
+	GasUsed      Gas
+	Logs         []Log
 }
 
+// Holds the data returned after executing a `Spawn` transaction.
 type SpawnReceipt struct {
 	Success         bool
 	Error           RuntimeError
@@ -66,6 +79,7 @@ type SpawnReceipt struct {
 	TouchedAccounts []Address
 }
 
+// Holds the data returned after executing a `Call` transaction.
 type CallReceipt struct {
 	Success         bool
 	Error           RuntimeError
